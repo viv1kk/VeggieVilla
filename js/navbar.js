2cc418b5df5a1863294f4f4464a01f0793fbea90
vivek <vivekkohli935@gmail.com>
2023-08-10 23:18:25 +0530

    Init

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+let navbarDrop = ()=>{
+    const x = document.getElementsByClassName('navbar-links')[0]    
+    x.classList.toggle('active')
+    console.log("Asd")
+  }
+
+  let userDrop = ()=>{
+    const x = document.getElementsByClassName('dropdown')[0]
+    x.classList.toggle('active')
+    console.log("Asd")
+  }
