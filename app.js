5a8e051996e7799f70a090267e1c24139d22d54b
vivek <vivekkohli935@gmail.com>
2023-08-13 01:50:43 +0530

    .

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+
+
+//Connet to MongoDB
+
+mongoose.connect('mongodb://localhost:27017/Veggies', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+
+// const login = require('./routes/login')
 let path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs")
 
-
+// app.use("/user", login)
 
 app.get('/', (req, res) => {
   res.render("index")
